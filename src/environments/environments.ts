85e9a3deeb41f49c88e04e858df30e16a437806d
John Doe <John@Doe.com>
2026-02-05 08:25:56 +0100

    chore: remove hardcoded API key

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environments = {
   production: false,
-  openWeatherApiKey: 'c88e6bd3d72d4de037dcf0820cb59ce3',
+  openWeatherApiKey: '',
   openWeatherApiUrl: 'https://api.openweathermap.org/data/2.5',
 };
