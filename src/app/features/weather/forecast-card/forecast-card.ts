80406f3992f4a775515df8d9c6e782175efdac5d
John Doe <John@Doe.com>
2026-02-06 09:57:27 +0100

    chore: round temperature

--- src/app/features/weather/forecast-card/forecast-card.ts
+++ src/app/features/weather/forecast-card/forecast-card.ts
@@ -1,10 +1,10 @@
 import { Component, input } from '@angular/core';
 import { forecastResponse } from '../../../core/models/forecast.models';
-import { DatePipe } from '@angular/common';
+import { DatePipe, DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-forecast-card',
-  imports: [DatePipe],
+  imports: [DatePipe, DecimalPipe],
   templateUrl: './forecast-card.html',
   styleUrl: './forecast-card.scss',
   standalone: true,
--- src/app/features/weather/weather-card/weather-card.ts
+++ src/app/features/weather/weather-card/weather-card.ts
@@ -1,9 +1,10 @@
 import { Component, input } from '@angular/core';
 import { WeatherResponse } from '../../../core/models/weather.model';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-weather-card',
-  imports: [],
+  imports: [DecimalPipe],
   templateUrl: './weather-card.html',
   styleUrl: './weather-card.scss',
   standalone: true,
